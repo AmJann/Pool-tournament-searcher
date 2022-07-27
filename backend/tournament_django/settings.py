e4d9a0459c342fb5802cba84dbdd6db2ac8bf02e
amjan <amjan91@gmail.com>
2022-07-27 19:38:42 -0400

    deploy

--- backend/tournament_django/settings.py
+++ backend/tournament_django/settings.py
@@ -111,10 +111,6 @@ DATABASES = {
 DATABASE_URL='postgres://tournamentuser:tournament@localhost:5432/tournament'
 
 
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
